660be71c73c335c44046a2fcda88bfb0e3c105d8
kelvys moura <kelvyscontato@gmail.com>
2018-06-17 03:52:27 -0300

    pagina home

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-UIkit.offcanvas("#offcanvas-usage").show();
+$(document).ready(function(){
+	let = anchor = window.location.hash;
+	if(anchor !== '' && anchor !== undefined){
+		$('section').addClass('uk-hidden');
+		let section = "section"+anchor;
+		$(section).removeClass('uk-hidden');
+	}
+
+	$('aside a').click(function(){
+		$('section').addClass('uk-hidden');
+		$('aside li').removeClass('uk-active');
+		let section = "section"+$(this).attr('href');
+		$(section).removeClass('uk-hidden');
+		$($(this).parent('li')).addClass('uk-active');
+	});
+});
